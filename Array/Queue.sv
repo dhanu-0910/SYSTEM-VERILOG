3d3553f272a4f070181683f5bf909df87fad6333
dhanu-0910 <dhanushiya952@gmail.com>
2026-03-28 10:00:50 +0530

    Create Queue.sv

---
+++ Array/Queue.sv
@@ -0,0 +1,33 @@
+module tb;
+  string d[$]={"dhanu","lavi","sowmi","tamizh"};
+  initial begin
+    foreach(d[i]) begin
+      $display("Names=%s",d[i]);
+    end
+    $display("Array=%p",d);
+    $display("Array=%p",d[1:2]);
+    $display("Array=%p",d[$:3]);
+    d[$+1]="thiri";
+    $display("Array=%p",d);
+    d[0]="sham";
+    $display("Array=%p",d);
+    d=d[1:$];
+    $display("Array=%p",d);
+    d=d[0:$-1];
+    $display("Array=%p",d);
+    d.insert(0,"dhanu");
+    $display("Array=%p",d);
+    d.delete(2);
+    $display("Array=%p",d);
+    $display("Array=%s",d.pop_front());
+    $display("Array=%s",d.pop_back());
+    $display("Array=%p",d);
+    d.push_front("dhanu");
+    $display("Array=%p",d);
+    d.push_back("tamizh");
+    $display("Array=%p",d);
+    $display("Array=%p",d.size());    
+    d={};
+    $display("Array=%p",d);
+  end
+endmodule
