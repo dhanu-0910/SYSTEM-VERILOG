f62c91f6e6315858bcbf32e456faba4fe894397c
dhanu-0910 <dhanushiya952@gmail.com>
2026-03-30 13:19:05 +0530

    Update Associate array.sv

--- Array/Associate array.sv
+++ Array/Associate array.sv
@@ -10,4 +10,26 @@ module tb;
     $display("b=%p",b);
     $display("c=%p",c);
   end
+  int d[int];
+  initial begin
+    int i;
+    d[5]=900;
+    d[2]=500;
+    d[7]=300;
+    d[10]=200;
+    $display("d=%p",d);
+    $display("d=%p",d.num());
+    $display("d=%p",d.size());
+    d.delete(2);
+    $display("d=%p",d);
+    $display("d=%p",d.exists(2));
+    d.first(i);
+    $display("d[%0d]=%0d",i,d[i]);
+    d.last(i);
+    $display("d[%0d]=%0d",i,d[i]);
+    d.prev(i);
+    $display("d[%0d]=%0d",i,d[i]);
+    d.next(i);
+    $display("d[%0d]=%0d",i,d[i]);    
+  end  
 endmodule
