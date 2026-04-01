441264de9a1105e0fd2f906db291fc5976403225
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-01 12:35:30 +0530

    Update For.sv

--- Looping statement/For.sv
+++ Looping statement/For.sv
@@ -10,3 +10,28 @@ module tb;
     $finish;
   end  
 endmodule
+//Another example
+module tb;
+  string a[5] = '{"dhanushiya", "lavanya", "sowmiya", "tamizh selvam", "sanjai"};
+  initial begin
+    for (int i = 0;  i < $size(a); i++) begin
+      int j;
+      j = a[i].len() - 1;   
+      a[i][j] = "0" ; 
+      $display("a[%0d]=%s", i, a[i]);
+    end
+  end
+endmodule
+//Another example
+module tb;
+  string a[5] = '{"dhanushiya", "lavanya", "sowmiya", "tamizh selvam", "sanjai"};
+  initial begin
+    for(int i=0;i<$size(a);i++) begin
+      int j;
+      j=a[i].len()-(i+1);
+      if(j>=0)
+        a[i][j]="0";
+      $display("%s",a[i]);
+    end
+  end
+endmodule
