3d6426c830b3d9ef3ab1b0d6154d01f144467aea
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-01 15:26:21 +0530

    Update Repeat.sv

--- Looping statement/Repeat.sv
+++ Looping statement/Repeat.sv
@@ -5,3 +5,16 @@ module tb;
     end
   end
 endmodule
+//Another example
+module tb;
+  bit clk;
+  always #10 clk=~clk;
+  initial begin
+    bit [2:0]a=6;
+    repeat(a) begin
+      @(posedge clk);
+      $display("(%0t) Repeat loom has ended=%0d",$time,a);
+    end
+    $finish;
+  end
+endmodule
