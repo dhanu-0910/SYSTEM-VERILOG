fb430fc2bef725753e05be75a6ea05302a794d69
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-01 12:46:01 +0530

    Update Foreach.sv

--- Looping statement/Foreach.sv
+++ Looping statement/Foreach.sv
@@ -9,5 +9,19 @@ module tb;
       end
    end
 endmodule
+//Another example (1D)
+module tb;
+  int a[5]='{1,2,3,4,5};
+  int product=1;
+  initial begin
+    foreach(a[i]) begin
+      $display("a[%0d]=%0d",i,a[i]);
+    end
+    foreach(a[i])begin
+      product=product*a[i];
+      $display("a[%0d]=%0d product=%0d",i,a[i],product);
+    end
+  end
+endmodule
 
     
