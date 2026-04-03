3267d6ad907aaf96abde2495c5d1df6fb63986dd
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-03 20:56:42 +0530

    Create Logical operator.sv

---
+++ Operators/Logical operator.sv
@@ -0,0 +1,19 @@
+module tb;
+  logic [3:0] a,b,c;
+  int x;
+  initial begin;
+    a= 15;
+    b= 9;
+    c= 1;
+    x = a||b;
+    $display("Logic OR = %0b",x);    
+    x = a&&b;
+    $display("Logic AND = %0b",x);    
+    x = !c;
+    $display("Logic Negation = %0b",x);
+    c=0;
+    x = !c;
+    $display("Logic Negation = %0b",x);        
+    
+  end
+endmodule
