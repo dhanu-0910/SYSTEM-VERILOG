ca7df38ae2368bcdaeb385c4b579930b194417ac
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-03 20:57:54 +0530

    Create Reduction operator.sv

---
+++ Operators/Reduction operator.sv
@@ -0,0 +1,19 @@
+module tb;
+  int a,b;
+  initial begin;
+    a= 5;
+    b = &a;
+    $display("and=%0b",b);
+    b = |a;
+    $display("or=%0b",b);
+    b = ^a;
+    $display("xor=%0b",b);
+    b = ~&a;
+    $display("nand=%0b",b);
+    b = ~|a;
+    $display("nor=%0b",b);
+    b = ~^a;
+    $display("xnor=%0b",b);
+   
+  end
+endmodule
