56f334adf256931a9e63a3e4ac44e6b5f6a85348
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-05 21:21:26 +0530

    Create Logical implication a<->b & b<->a.sv

---
+++ Operators/Logical implication a<->b & b<->a.sv
@@ -0,0 +1,29 @@
+module tb;
+  logic [4:0] a,b;
+  initial begin;
+    a= 0;//(a<->b=((a->b)&&(b->a))
+    b= 0;
+    if(a<->b)
+      $display("True (%0d)",(a<->b));
+    else
+      $display("False (%0d)",(a<->b));    
+    a= 0;
+    b= 1;
+    if(a<->b)
+      $display("True (%0d)",(a<->b));
+    else
+      $display("False (%0d)",(a<->b));    
+    a= 1;
+    b= 0;
+    if(a<->b)
+      $display("True (%0d)",(a<->b));
+    else
+      $display("False (%0d)",(a<->b));    
+    a= 1;
+    b= 1;
+    if(a<->b)
+      $display("True (%0d)",(a<->b));
+    else
+      $display("False (%0d)",(a<->b));    
+  end
+endmodule
