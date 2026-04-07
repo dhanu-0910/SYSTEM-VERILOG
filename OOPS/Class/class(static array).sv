d81b74c91e788d48db652f9f165b9481d6cdb670
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-07 11:48:04 +0530

    Update class(static array).sv

--- OOPS/Class/class(static array).sv
+++ OOPS/Class/class(static array).sv
@@ -16,6 +16,26 @@ module tb;
   end
   end
 endmodule
+//without constructor
+class packet;
+  int count;
+  function void counter(int c);
+    count = c;
+  endfunction
+endclass
+
+module tb;
+  packet a[10];
+  initial begin
+    for(int i=0;i<10;i++) begin
+      a[i] = new();          
+      a[i].counter(i*5);     
+    end
+    for(int i=0;i<10;i++) begin
+      $display("a[%0d]=%0d", i, a[i].count);
+    end
+  end
+endmodule
 //Output
 a[0]=0
 a[1]=5
