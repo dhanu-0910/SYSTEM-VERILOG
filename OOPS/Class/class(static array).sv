d8572ccab721f0f1ac7f4b0a90b0f7fbe025c875
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-07 11:47:00 +0530

    Create class(static array).sv

---
+++ OOPS/Class/class(static array).sv
@@ -0,0 +1,29 @@
+class packet;
+  int count;
+  function new(int c);
+    count=c;
+  endfunction
+endclass
+
+module tb;
+  packet a[10];
+  initial begin
+    for(int i=0;i<10;i++)begin
+    a[i]=new(i*5);
+  end
+    for(int i=0;i<10;i++) begin
+      $display("a[%0d]=%0d",i,a[i].count);
+  end
+  end
+endmodule
+//Output
+a[0]=0
+a[1]=5
+a[2]=10
+a[3]=15
+a[4]=20
+a[5]=25
+a[6]=30
+a[7]=35
+a[8]=40
+a[9]=45
