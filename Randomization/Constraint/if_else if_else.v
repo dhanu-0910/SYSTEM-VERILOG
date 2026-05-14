8ec55d073871737d76540e620f73e231e49c0759
dhanu-0910 <dhanushiya952@gmail.com>
2026-05-14 18:20:02 +0530

    Create if_else if_else.v

---
+++ Randomization/Constraint/if_else if_else.v
@@ -0,0 +1,43 @@
+class data;
+  rand bit[6:0] mark;
+  rand enum {BEST, AVERAGE, WORST} scale;
+  
+  constraint scale_c {
+    if(scale==BEST) 
+    {
+      mark>=90; 
+      mark<=100;
+    }
+      else if(scale==AVERAGE) 
+      {
+        mark<=89; 
+        mark>=40;
+      } 
+        else {
+          mark<=39;
+        }
+  }
+    
+endclass
+
+module tb;
+  data d;
+  initial begin
+    d=new();
+    repeat(10)begin
+      d.randomize();
+      $display("scale=%s mark=%0d",d.scale.name(),d.mark);
+    end
+  end
+endmodule
+    //Output
+    scale=WORST mark=24
+scale=WORST mark=36
+scale=BEST mark=90
+scale=BEST mark=97
+scale=BEST mark=97
+scale=BEST mark=96
+scale=AVERAGE mark=77
+scale=WORST mark=8
+scale=AVERAGE mark=70
+scale=BEST mark=94
