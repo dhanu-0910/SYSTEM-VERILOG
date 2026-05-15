9e4b84f68946da1356b444ff3cf945b4367b368c
dhanu-0910 <dhanushiya952@gmail.com>
2026-05-15 12:09:32 +0530

    Create inheritance with diff constraint name.sv

---
+++ Randomization/Constraint/inheritance with diff constraint name.sv
@@ -0,0 +1,39 @@
+//If ranges conflict → randomization fails
+//If ranges overlap → randomization succeeds i.e., child doesn't overrides parent
+class parent;
+  rand bit [5:0]data;
+  constraint data_c {data>5; data<15;}
+endclass
+
+class child extends parent;
+  constraint dataaa_c {data inside {[10:30]};}
+endclass
+
+module tb;
+  parent p;
+  child c;
+  initial begin
+    p=new();
+    c=new();
+    repeat(5) begin
+      p.randomize();
+      $display("BASE: data=%0d",p.data);
+    end
+    repeat(5) begin
+      c.randomize();
+      $display("CHILD: data=%0d",c.data);
+    end
+  end
+endmodule
+//Output
+BASE: data=6
+BASE: data=14
+BASE: data=11
+BASE: data=13
+BASE: data=10
+CHILD: data=11
+CHILD: data=12
+CHILD: data=13
+CHILD: data=11
+CHILD: data=14
+    
