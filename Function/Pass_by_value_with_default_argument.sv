86b0d84c71f830efaf211729671e855ced01de9a
dhanu-0910 <dhanushiya952@gmail.com>
2026-06-03 13:05:04 +0530

    Create Pass_by_value_with_default_argument.sv

---
+++ Function/Pass_by_value_with_default_argument.sv
@@ -0,0 +1,24 @@
+module test;
+
+  function void modify(int a=10);
+    a = a + 10;
+    $display("Inside Function: a = %0d", a);
+  endfunction
+
+  int x = 20;
+
+  initial begin
+    $display("Before Function Call: x = %0d", x);
+
+    modify();
+    modify(x);
+
+    $display("After Function Call: x = %0d", x);
+  end
+
+endmodule
+//Output
+Before Function Call: x = 20
+Inside Function: a = 20
+Inside Function: a = 30
+After Function Call: x = 20
