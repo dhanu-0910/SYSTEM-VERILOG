74a38e748cf518078e92c8112b1c0e94488c036a
dhanu-0910 <dhanushiya952@gmail.com>
2026-06-05 12:00:13 +0530

    Create Calling_fn_inside_task.sv

---
+++ Task/Calling_fn_inside_task.sv
@@ -0,0 +1,25 @@
+module func_from_task;
+
+  task task_sum;
+    #1;
+    $display( "Time= %0t",$time);
+    $display("Inside a task");
+    #1;
+    void'(function_sum());
+  endtask
+
+  function function_sum;
+    $display( "Time= %0t",$time);
+    $display( "Inside a function",$time); 
+  endfunction
+  
+  initial begin
+    task_sum;
+  end
+
+endmodule
+//Output
+Time= 1
+Inside a task
+Time= 2
+Inside a function 
