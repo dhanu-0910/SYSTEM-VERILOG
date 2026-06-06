2d317a4beef32765cd9fac771e7de90d7a1b3a2b
dhanu-0910 <dhanushiya952@gmail.com>
2026-06-06 10:14:17 +0530

    Create event_triggered_sametime_waiting_for_event_trigger.sv

---
+++ Interprocess_communication/Event/event_triggered_sametime_waiting_for_event_trigger.sv
@@ -0,0 +1,27 @@
+module event_example();
+  event e1;
+  
+  task A();
+    $display("Time= %0t: Before triggering event e1", $time);
+    @e1;
+    $display("Time= %0t: After triggering event e1", $time);
+  endtask
+  
+  task B();
+    $display("Time= %0t: waiting for the event e1", $time);
+    ->e1;
+    $display("Time= %0t: event e1 is triggered", $time);
+  endtask
+
+  initial begin
+    fork
+      A();
+      B();
+    join
+  end
+endmodule
+//Output
+Time= 0: Before triggering event e1
+Time= 0: waiting for the event e1
+Time= 0: event e1 is triggered
+Time= 0: After triggering event e1
