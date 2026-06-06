4a7805b936c80f7866f57a0503314f6270f08641
dhanu-0910 <dhanushiya952@gmail.com>
2026-06-06 10:34:49 +0530

    Create Both_@_and_wait(trigger).sv

---
+++ Interprocess_communication/Event/Both_@_and_wait(trigger).sv
@@ -0,0 +1,38 @@
+module event_example();
+  event e1;
+  
+  task A();
+    $display("@%0t: Process A: Before triggering event e1", $time);
+    #10;
+    ->e1;
+    $display("@%0t: Process A: After triggering event e1", $time);
+  endtask
+  
+  task B();
+    $display("@%0t: Process B: waiting for the event e1 using @", $time);
+    #10;
+    @e1;
+    $display("@%0t: Process B: event e1 is triggered using @", $time);
+  endtask
+
+  task C();
+    $display("@%0t: Process C: waiting for the event e1 using wait(e1.triggered)", $time);
+    #10;
+    wait(e1.triggered);
+    $display("@%0t: Process C: event e1 is triggered using wait(e1.triggered)", $time);
+  endtask
+  
+  initial begin
+    fork
+      A();
+      B();
+      C();
+    join
+  end
+endmodule
+//Output
+@0: Process A: Before triggering event e1
+@0: Process B: waiting for the event e1 using @
+@0: Process C: waiting for the event e1 using wait(e1.triggered)
+@10: Process A: After triggering event e1
+@10: Process C: event e1 is triggered using wait(e1.triggered)
