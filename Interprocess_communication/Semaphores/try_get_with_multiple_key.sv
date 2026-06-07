a2368bc9253183deb7fa685543a2ae3fa288f5c9
dhanu-0910 <dhanushiya952@gmail.com>
2026-06-07 19:02:44 +0530

    Create try_get_with_multiple_key.sv

---
+++ Interprocess_communication/Semaphores/try_get_with_multiple_key.sv
@@ -0,0 +1,47 @@
+module semaphore_example();
+  semaphore sem = new(3);
+  
+  task process_A();
+    if (sem.try_get()) begin
+      $display("Time= %0t: process_A: Key received",$time);
+
+      $display("Time= %0t: process_A started",$time);
+      #5;
+      $display("Time= %0t: process_A completed",$time);
+
+      sem.put();
+    end
+    else begin
+      $display("Time= %0t: process_A: Key is not available",$time);
+    end
+  endtask
+  
+  task process_B();
+    if (sem.try_get()) begin
+      $display("Time= %0t: process_B: Key received",$time);
+
+      $display("Time= %0t: process_B started",$time);
+      #4;
+      $display("Time= %0t: process_B completed",$time);
+
+      sem.put();
+    end
+    else begin
+      $display("Time= %0t: process_B: Key is not available",$time);
+    end
+  endtask
+
+  initial begin
+    fork
+      process_A();
+      process_B();
+    join
+  end
+endmodule
+//Output
+Time= 0: process_A: Key received
+Time= 0: process_A started
+Time= 0: process_B: Key received
+Time= 0: process_B started
+Time= 4: process_B completed
+Time= 5: process_A completed
