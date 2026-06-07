708e12724bed9f385895c74620e16cd33f83ce96
dhanu-0910 <dhanushiya952@gmail.com>
2026-06-07 19:01:09 +0530

    Create sempahore_with_only_write.sv

---
+++ Interprocess_communication/Semaphores/sempahore_with_only_write.sv
@@ -0,0 +1,31 @@
+module sem();
+  semaphore s=new(3);
+  
+  task write();
+    s.get(3);
+    $display("Time= %0t: Before writing starts",$time);
+    #5;
+    $display("Time= %0t: Writing completed",$time);
+    s.put(1);
+  endtask
+  
+  task read();
+    s.get(2);
+    $display("Time= %0t: Before reading starts",$time);
+    #5;
+    $display("Time= %0t: Reading completed",$time);
+    s.put(2);
+  endtask
+  
+  initial begin
+    
+    fork
+      write();
+      read();
+    join
+    
+  end
+endmodule
+//Output
+Time= 0: Before writing starts
+Time= 5: Writing completed
