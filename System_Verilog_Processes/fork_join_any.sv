585ebe27b8e3026f50ec3bc1e1956d841c28e55a
dhanu-0910 <dhanushiya952@gmail.com>
2026-06-09 09:45:49 +0530

    Create fork_join_any.sv

---
+++ System_Verilog_Processes/fork_join_any.sv
@@ -0,0 +1,27 @@
+module fork_join_example;
+
+  initial begin
+    $display("Simulation Started at %0t", $time);
+
+    fork
+      begin : task1
+        #5;
+        $display("Task1 completed at %0t", $time);
+      end
+
+      begin : task2
+        #10;
+        $display("Task2 completed at %0t", $time);
+      end
+
+      begin : task3
+        #15;
+        $display("Task3 completed at %0t", $time);
+      end
+    join_any
+
+    $display("All Tasks Completed at %0t", $time);
+
+  end
+
+endmodule
