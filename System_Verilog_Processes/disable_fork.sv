da21ff2be452e7304818aa5cf84d8b2c6ed1771d
dhanu-0910 <dhanushiya952@gmail.com>
2026-06-09 09:50:57 +0530

    Create disable_fork.sv

---
+++ System_Verilog_Processes/disable_fork.sv
@@ -0,0 +1,32 @@
+module fork_join_example;
+
+  initial begin
+    $display("Simulation Started at %0t", $time);
+
+    fork
+      begin : task1
+        #5;
+        $display("Task1 completed at %0t", $time);
+      end
+
+      begin : task2
+        #10;
+        $display("Task2 completed at %0t", $time);
+      end
+
+      begin : task3
+        #15;
+        $display("Task3 completed at %0t", $time);
+      end
+    join_any
+    disable fork;
+    $display("All Tasks Completed at %0t", $time);
+
+  end
+
+endmodule
+//Output
+Simulation Started at 0
+Task1 completed at 5
+All Tasks Completed at 5
+      
