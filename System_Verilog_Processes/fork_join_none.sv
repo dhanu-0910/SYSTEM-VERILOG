aeb21ce49b02391f2c50e11831d71347633b886c
dhanu-0910 <dhanushiya952@gmail.com>
2026-06-09 09:47:34 +0530

    Create fork_join_none.sv

---
+++ System_Verilog_Processes/fork_join_none.sv
@@ -0,0 +1,33 @@
+module fork_join_example;
+
+  initial begin
+    $display("Simulation Started at %0t", $time);
+
+    fork
+      begin : task1
+        #5;
+        $display("Task1 completed at %0t", $time);
+      end
+
+      begin : task2
+        #10;
+        $display("Task2 completed at %0t", $time);
+      end
+
+      begin : task3
+        #15;
+        $display("Task3 completed at %0t", $time);
+      end
+    join_none
+
+    $display("All Tasks Completed at %0t", $time);
+
+  end
+
+endmodule
+//Output
+Simulation Started at 0
+All Tasks Completed at 0
+Task1 completed at 5
+Task2 completed at 10
+Task3 completed at 15
