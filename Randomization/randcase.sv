3ff97975d669135c8f4ebfcc9d109a3ffbd694c4
dhanu-0910 <dhanushiya952@gmail.com>
2026-06-09 09:37:42 +0530

    Update randcase.sv

--- Randomization/randcase.sv
+++ Randomization/randcase.sv
@@ -59,3 +59,44 @@ module tb;
   end
 
 endmodule
+//Output
+Selected 9
+Selected 9
+Selected 6
+Selected 6
+Selected 9
+Selected 9
+Selected 6
+Selected 4
+Selected 6
+Selected 9
+Selected 9
+Selected 6
+Selected 9
+Selected 6
+Selected 9
+Selected 6
+Selected 9
+Selected 9
+Selected 6
+Selected 1
+--------------------------------
+Weight = 1
+Count  = 1
+Expected Probability = 0.050000
+Observed Probability = 0.050000
+--------------------------------
+Weight = 4
+Count  = 1
+Expected Probability = 0.200000
+Observed Probability = 0.050000
+--------------------------------
+Weight = 6
+Count  = 8
+Expected Probability = 0.300000
+Observed Probability = 0.400000
+--------------------------------
+Weight = 9
+Count  = 10
+Expected Probability = 0.450000
+Observed Probability = 0.500000
