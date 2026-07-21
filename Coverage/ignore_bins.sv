047709bde2d48c1211b157e4b00e343a9f1ca1e1
dhanu-0910 <dhanushiya952@gmail.com>
2026-07-21 16:34:36 +0530

    Create ignore_bins.sv

---
+++ Coverage/ignore_bins.sv
@@ -0,0 +1,19 @@
+module f_c;
+  bit [3:0]a;
+  
+  covergroup c_grp;
+    option.per_instance=1;
+    c1: coverpoint a{
+      ignore_bins b= {10,11,12};}
+  endgroup
+  
+  initial begin
+    c_grp c;
+    c=new();
+    repeat(25) begin
+      a=$urandom;
+      c.sample();
+      $display("a=%0d",a);
+    end
+  end
+endmodule
